dc9762277614783425579597f18213920d831732
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-04-09 12:28:03 -0300

    Add Crud

--- src/routes/PessoasRoute.js
+++ src/routes/PessoasRoute.js
@@ -4,5 +4,9 @@ const pessoaController = new PessoasController();
 const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.selecionaTodos(req, res));
+// router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req, res));
+// router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
+// router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
 
 module.exports = router;
